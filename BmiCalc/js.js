9de7405dda7fb037cb469686fad9bb3a0c5ebf0f
DINHXUANHOA <xuanhoa1697@gmail.com>
2021-08-21 02:05:08 +0700

    up2

--- BmiCalc/js.js
+++ BmiCalc/js.js
@@ -2,16 +2,26 @@ let btnRenext = document.querySelector(".btn-renext")
 let btnNext = document.querySelector(".btn-next")
 let calc = document.querySelector(".calc")
 let container = document.querySelector(".container")
+let body = document.body
 
 btnNext.addEventListener("click",function(){
     calc.style.right = "110px"
     container.style.left = "-550px"
     btnRenext.style.color = "rgb(40, 235, 1)"
     btnNext.style.color = "black"
+    body.style.cursor = "wait"
+    setTimeout(function(){
+        body.style.cursor = "pointer"
+    },800)
+    
 })
 btnRenext.addEventListener("click",function(){
     calc.style.right = "-300px"
     container.style.left = "30px"
     btnNext.style.color = "rgb(40, 235, 1)"
     btnRenext.style.color = "black"
+    body.style.cursor = "wait"
+    setTimeout(function(){
+        body.style.cursor = "pointer"
+    },800)
 })
