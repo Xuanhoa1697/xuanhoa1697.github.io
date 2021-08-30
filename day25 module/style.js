c1393ec302c82fb175f6b1a8f2194a781b361975
DINHXUANHOA <xuanhoa1697@gmail.com>
2021-08-31 02:13:45 +0700

    module2

--- day25 module/style.js
+++ day25 module/style.js
@@ -1,37 +1,30 @@
 //export cho index.js
-export{email,password,iconPadding}
+export { email, password, iconPadding };
 
 //Style input
-let email = document.querySelector(".email")
-let iconEmail = document.querySelector(".icon-email")
-let password = document.querySelector(".password")
-let iconPass = document.querySelector(".icon-pass")
+let email = document.querySelector(".email");
+let iconEmail = document.querySelector(".icon-email");
+let password = document.querySelector(".password");
+let iconPass = document.querySelector(".icon-pass");
 
-email.addEventListener("click",()=>{
-    email.style.paddingLeft = "30px"
-    iconEmail.style.transform = "translateX(0)"
-    password.style.paddingLeft = "13px"
-    iconPass.style.transform = "translateX(-25px)"
-    email.style.borderColor = "#ED2553"
-}
-)
-password.addEventListener("click",()=>{
-    password.style.paddingLeft = "30px"
-    iconPass.style.transform = "translateX(0)"
-    email.style.paddingLeft = "13px"
-    iconEmail.style.transform = "translateX(-25px)"
-    password.style.borderColor = "#ED2553"
-}
-)
+email.addEventListener("click", () => {
+  email.style.paddingLeft = "30px";
+  iconEmail.style.transform = "translateX(0)";
+  password.style.paddingLeft = "13px";
+  iconPass.style.transform = "translateX(-25px)";
+  email.style.borderColor = "#ED2553";
+});
+password.addEventListener("click", () => {
+  password.style.paddingLeft = "30px";
+  iconPass.style.transform = "translateX(0)";
+  email.style.paddingLeft = "13px";
+  iconEmail.style.transform = "translateX(-25px)";
+  password.style.borderColor = "#ED2553";
+});
 
-
-let iconPadding = ()=>{
-    password.style.paddingLeft = "13px"
-    iconPass.style.transform = "translateX(-25px)"
-    email.style.paddingLeft = "13px"
-    iconEmail.style.transform = "translateX(-25px)"
-    
-}
-
-
-
+let iconPadding = () => {
+  password.style.paddingLeft = "13px";
+  iconPass.style.transform = "translateX(-25px)";
+  email.style.paddingLeft = "13px";
+  iconEmail.style.transform = "translateX(-25px)";
+};
--- day25 module/validate.js
+++ day25 module/validate.js
@@ -1,41 +1,44 @@
-// export cho index.js
-export {validate}
+// Export cho index.js
+export { validate };
 //Regex xác thực
 let regexEmail1 = /[a-z]+[0-9]+@[a-z]+.[a-z]{2,}$/;
 let regexEmail2 = /[a-z]+@[a-z]+.[a-z]{2,}$/;
 let regexPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/; //8 ký tư tói thiểu 1 số 1 chữ
 let count;
-let count1 = 0
-let count2 = 0
+let count1 = 0;
+let count2 = 0;
 //Xử lý validate
-let validate = (...node)=>{
-    for(let i =0;i<=node.length;i++){
-        if((regexEmail1.test(node[0])== true || regexEmail2.test(node[0])==true) && (regexPass.test(node[1]) == true )){
-            node[4].textContent = "Waiting"
-            count = true
-        }else{
-            node[2].textContent = "Nhập lại Email!"
-            node[3].textContent = "Nhập lại Password!"
+let validate = (...node) => {
+  for (let i = 0; i <= node.length; i++) {
+    if (
+      (regexEmail1.test(node[0]) == true ||
+        regexEmail2.test(node[0]) == true) &&
+      regexPass.test(node[1]) == true
+    ) {
+      node[4].textContent = "Waiting";
+      count = true;
+    } else {
+      node[2].textContent = "Nhập lại Email!";
+      node[3].textContent = "Nhập lại Password!";
+    }
+  }
+  setTimeout(() => {
+    node[2].textContent = "";
+    node[3].textContent = "";
+  }, 2000);
+  if (count == true) {
+    let time = setInterval(() => {
+      node[4].textContent += ".";
+      count1++;
+      if (count1 >= 4) {
+        node[4].textContent = "Waiting";
+        count1 = 0;
+        count2++;
+        if (count2 >= 2) {
+          node[4].textContent = "Connected ✔";
+          clearInterval(time);
         }
-    }
-    setTimeout(()=>{
-        node[2].textContent = ""
-        node[3].textContent = ""
-    },2000)
-    if(count== true){
-        let time = setInterval(()=>{
-            node[4].textContent +="." 
-            count1++
-            if(count1===4){
-                node[4].textContent ="Waiting"
-                count1=0
-                count2++
-                if(count2===2){
-                    node[4].textContent ="Connected ✔"
-                    clearInterval(time)
-                }
-            }  
-        },500)
-    }
-}
-
+      }
+    }, 500);
+  }
+};
