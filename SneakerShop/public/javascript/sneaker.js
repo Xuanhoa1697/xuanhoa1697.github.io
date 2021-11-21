46819cdeda9bed7c74e138b21a8b6bc4db6b73b8
DINHXUANHOA <xuanhoa1697@gmail.com>
2021-11-22 01:58:35 +0700

    lân 20

--- SneakerShop/public/javascript/sneaker.js
+++ SneakerShop/public/javascript/sneaker.js
@@ -1,16 +1,15 @@
 $(()=>{
     $(`.slider-range`).slider({
-        range:false,
-        min:0,
-        max:3500000,
-        step:100000,
-        value:1750000,
-        slide:(event,data)=>{
-            let price = data.value
-            $(`.max`).text(price.toLocaleString("vi-VN") + "đ")
-            $(`.span-color`).css({
-                background:`linear-gradient(90deg, #f54f29 ${((data.value / 3500000) * 100) + "%"}, rgb(207, 207, 207) ${((data.value / 3500000) * 100) + "%"})`
-            })
+        range: true,
+        min: 0,
+        max: 3500000,
+        step: 100000,
+        values: [ 0, 3500000 ],
+        slide: function( event, ui ) {
+            let max = ui.values[ 1 ].toLocaleString('vi-VN');
+            let min = ui.values[ 0 ].toLocaleString('vi-VN');
+            let price = ( min + "đ" + " - " + max + "đ")
+            $(".max" ).text( price );
         }
     })
     let number = 0
@@ -21,10 +20,22 @@ $(()=>{
         number--
         if(number <= 0){
             number =0
+            $(".prev .bi-chevron-left").css({
+                color:"#cacaca"
+            })
+            $(".next .bi-chevron-right").css({
+                color:"#494949"
+            })
+        }else{
+            $(".prev .bi-chevron-left").css({
+                color:"#494949"
+            })
+            $(".next .bi-chevron-right").css({
+                color:"#494949"
+            })
         }
         $(".link-page").removeClass("active-page")
         $(page[number]).addClass("active-page")
-        event.preventDefault()
         console.log("lol");
     })
     $(`.next`).on("click",()=>{
@@ -33,18 +44,32 @@ $(()=>{
         number++
         if(number >= 3){
             number = 3
+            $(".prev .bi-chevron-left").css({
+                color:"#494949"
+            })
+            $(".next .bi-chevron-right").css({
+                color:"#cacaca"
+            })
+        }else{
+            $(".prev .bi-chevron-left").css({
+                color:"#494949"
+            })
+            $(".next .bi-chevron-right").css({
+                color:"#494949"
+            })
         }
         $(".link-page").removeClass("active-page")
         $(page[number]).addClass("active-page")
-        event.preventDefault()
         console.log(number);
     })
-    for(let i = 0;i <= page.length;i++){
-        $(page[i]).on("click",()=>{
+    let arrPage = Array.from($(".direct-page"))
+    let url = new URLSearchParams(window.location.search);
+        let numberPage1 = url.get("_page") || 1;
+        for(let i = 0;i < numberPage1;i++){
             $(".link-page").removeClass("active-page")
             $(page[i]).addClass("active-page")
-            number = i
-            event.preventDefault()
-        })
-    }
+            $(arrPage[numberPage1 - 1]).css({
+                opacity:0.2
+            })
+        }
 })
